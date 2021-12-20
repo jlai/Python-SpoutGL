af47e45936dc230cea373947a4fcaf21656a66b0
Jason Lai <jlai@users.noreply.github.com>
2021-12-19 17:21:09 -0800

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -10,42 +10,46 @@ import os.path
 
 import sys
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 is_64bits = sys.maxsize > 2**32
 
 spout_lib_base_dir = "vendor/SpoutGL/Binaries"
-spout_lib_dir = os.path.join(spout_lib_base_dir, "x64" if is_64bits else "Win32")
+spout_lib_dir = os.path.join(
+    spout_lib_base_dir, "x64" if is_64bits else "Win32")
 
 ext_modules = [
     Pybind11Extension("SpoutGL._spoutgl",
-        # Note:
-        #   Sort input source files if you glob sources to ensure bit-for-bit
-        #   reproducible builds (https://github.com/pybind/python_example/pull/53)
-        ["src/PySpoutGL.cpp"],
-        
-        # Pass in the version to the compiled code
-        define_macros = [('VERSION_INFO', __version__)],
-        
-        cxx_std = 17,
-        include_dirs = ['vendor/SpoutGL'],
-        libraries = ['spout'],
-        library_dirs = [spout_lib_dir],
-        
-        # Generate VS pdb symbols for debugging
-        #extra_link_args=["/DEBUG:FULL"]
-    ),
+                      # Note:
+                      #   Sort input source files if you glob sources to ensure bit-for-bit
+                      #   reproducible builds (https://github.com/pybind/python_example/pull/53)
+                      ["src/PySpoutGL.cpp"],
+
+                      # Pass in the version to the compiled code
+                      define_macros=[('VERSION_INFO', __version__)],
+
+                      cxx_std=17,
+                      include_dirs=['vendor/SpoutGL'],
+                      libraries=['spout'],
+                      library_dirs=[spout_lib_dir],
+
+                      # Generate VS pdb symbols for debugging
+                      # extra_link_args=["/DEBUG:FULL"]
+                      ),
 ]
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+
 class CopyDllsAndBuildCommand(BuildCommand):
     """Copies arch-specific DLLs to the SpoutGL package directory"""
 
     def run(self):
-        shutil.copyfile(os.path.join(spout_lib_dir, "Spout.dll"), "src/SpoutGL/Spout.dll")
+        shutil.copyfile(os.path.join(spout_lib_dir, "Spout.dll"),
+                        "src/SpoutGL/Spout.dll")
         super().run()
+
 
 setup(
     name="SpoutGL",
@@ -56,7 +60,7 @@ setup(
     description="Wrapper around Spout frame streaming library for Windows",
     long_description=README,
     long_description_content_type="text/markdown",
-    
+
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Operating System :: Microsoft :: Windows",
@@ -64,7 +68,7 @@ setup(
         "Topic :: Multimedia :: Graphics :: Capture",
         "Topic :: Multimedia :: Graphics :: Viewers"
     ],
-    
+
     ext_modules=ext_modules,
     extras_require={"test": []},
     cmdclass={
