e61e2aefb7358fa09678aec2b25e3d38aee19b90
Jason Lai <jlai@users.noreply.github.com>
2025-10-27 22:38:50 -0700

    Add Python 3.14 and remove Python 3.8 (EOL) Update pybind to 3.0.1

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         "build": CopyDllsAndBuildCommand
     },
     zip_safe=False,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 
     packages=["SpoutGL"],
     package_dir={"": "src"},
